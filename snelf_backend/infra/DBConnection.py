499e0d9bec2686484f2506dc46a4448d06a693e9
kassiopereirabm <kassio.lima.p@gmail.com>
2023-03-24 21:32:48 -0300

    Update DBConnection.py

--- snelf_backend/infra/DBConnection.py
+++ snelf_backend/infra/DBConnection.py
@@ -15,11 +15,20 @@ class DBConnection:
             host = "snelf-postgres"
             port = "5432"
 
-            try:
-                cls._self.connection = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
-            except Exception as e:
-                cls._self.connection = psycopg2.connect(database=database, user=user, password=password, host="localhost", port="54320")
+            def tryToConnect():
+                try:
+                    cls._self.connection = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+                    print("back conectado ao banco pela porta 5432")
+                except Exception as e:
+                    try:
+                        print("conexão com o banco na porta 5432 falhou")
+                        cls._self.connection = psycopg2.connect(database=database, user=user, password=password, host="localhost", port="54320")
+                        print("back conectado ao banco pela porta 54320")
+                    except Exception as e2:
+                        print("conexão com o banco na porta 54320 falhou")
+                        tryToConnect()
 
+            tryToConnect()
             cls._self.connection.autocommit = True
             cls._self.cursor = cls._self.connection.cursor()
 
